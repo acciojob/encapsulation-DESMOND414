35a4fd2e9870385c0f85c039fd280e1fb3bfa6a0
Algani Uthej <69247162+DESMOND414@users.noreply.github.com>
2023-04-18 23:40:11 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
      public static void main(String[] args) {
-        RWOnly obj = new RWOnly(
+        RWOnly obj = new RWOnly();
         obj.setName("John");
         String name = obj.getName();
         System.out.println(name);
